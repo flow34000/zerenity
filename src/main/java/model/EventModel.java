372f10a3183ad776766a6b62e6f8e7c489524eb9
Florent Guillaume <guillaume.florent@live.fr>
2015-11-22 12:27:47 +0100

    fix nested transac

--- src/main/java/model/EventModel.java
+++ src/main/java/model/EventModel.java
@@ -18,31 +18,31 @@ public class EventModel extends AbstractModel<Event, Short> {
 	}
 	
 	public Short save(JSONEvent e) throws Exception{
+		Event event = new Event();
+		event.setEventName(e.getEventName());
+		event.setEventPrice(e.getEventPrice());
+		event.setEventStartDate(e.getEventStartDate());
+		event.setEventEndDate(e.getEventEndDate());
+		
+		if(e.getRepetitionId() != 0){
+			RepetitionModel rm = new RepetitionModel();
+			Repetition r = rm.get(e.getRepetitionId());
+			event.setRepetition(r);
+		}
+		UserModel um = new UserModel();
+		User u = um.get(e.getUserId());
+		event.setUser(u);
+		ActivityModel am = new ActivityModel();
+		Activity a = am.get(e.getActivityId());
+		event.setActivity(a);
+		RoomModel rom = new RoomModel();
+		Room room = rom.get(e.getRoomId());
+		event.setRoom(room);
+		
 		Session session = HibernateUtil.currentSession();
 		try{
 			Transaction tx = session.beginTransaction();
 			try{
-				Event event = new Event();
-				event.setEventName(e.getEventName());
-				event.setEventPrice(e.getEventPrice());
-				event.setEventStartDate(e.getEventStartDate());
-				event.setEventEndDate(e.getEventEndDate());
-				
-				if(e.getRepetitionId() != 0){
-					RepetitionModel rm = new RepetitionModel();
-					Repetition r = rm.get(e.getRepetitionId());
-					event.setRepetition(r);
-				}
-				UserModel um = new UserModel();
-				User u = um.get(e.getUserId());
-				event.setUser(u);
-				ActivityModel am = new ActivityModel();
-				Activity a = am.get(e.getActivityId());
-				event.setActivity(a);
-				RoomModel rom = new RoomModel();
-				Room room = rom.get(e.getRoomId());
-				event.setRoom(room);
-				
 				Short eventId = (Short)session.save(event);		
         		tx.commit();
         		return eventId;
